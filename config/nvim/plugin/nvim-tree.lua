eb3b52e864c3365bd07e69b2db1fe63f247b50c6
manuelgcsousa <manuel.sousa@tonicapp.com>
2024-07-23 10:05:36 +0100

    [nvim] restore nvim-tree

--- config/nvim/plugin/nvim-tree.lua
+++ config/nvim/plugin/nvim-tree.lua
@@ -1,51 +1,43 @@
--- require('nvim-web-devicons').setup({
---   override = {
---     toml = {
---       icon = "󰬛",
---       color = "#9c4221",
---       cterm_color = "124",
---       name = "Toml",
---     },
---   };
--- })
--- 
--- local function on_attach(bufnr)
---   local api = require('nvim-tree.api')
--- 
---   local function opts(desc)
---     return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
---   end
--- 
---   api.config.mappings.default_on_attach(bufnr)
--- 
---   vim.keymap.set('n', '<C-e>', api.tree.close, opts('Close'))
--- end
--- 
--- require('nvim-tree').setup({
---   disable_netrw = true,
---   hijack_netrw = true,
---   open_on_tab = false,
---   update_cwd = true,
---   update_focused_file = {
---     enable = true
---   },
---   view = {
---     width = 45,
---     side = 'left'
---   },
---   on_attach = on_attach
--- })
-
-
-
-require("neo-tree").setup({
-  popup_border_style = "single",
-  enable_diagnostics = false,
-  filesystem = {
-    filtered_items = {
-      hide_dotfiles = false,
-    }
-  }
+require('nvim-web-devicons').setup({
+  override = {
+    toml = {
+      icon = "󰬛",
+      color = "#9c4221",
+      cterm_color = "124",
+      name = "Toml",
+    },
+  };
 })
 
-vim.api.nvim_set_keymap('n', '<C-e>', ':Neotree toggle<CR>', { noremap = true, silent = true })
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', '<C-e>', api.tree.close, opts('Close'))
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
+require('nvim-tree').setup({
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_tab = false,
+  update_cwd = true,
+  update_focused_file = {
+    enable = true
+  },
+  view = {
+    width = 45,
+    side = 'left',
+  },
+  renderer = {
+    indent_markers = {
+      enable = true,
+    },
+  },
+  on_attach = on_attach
+})
